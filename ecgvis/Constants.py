560bb9fc1d517b9568aace8fadb7a2e42e9c7b6a
sfcaracciolo <santiago.caracciolo@gmail.com>
2021-12-02 21:24:20 -0300

    add light colors

--- ecgvis/Constants.py
+++ ecgvis/Constants.py
@@ -1,8 +1,14 @@
 import numpy as np
 
+INT_NAN = 2147483647
+
+
 AMBER = np.array((1.,.75,0.,.5), dtype=np.float32)
 LIGHT_AMBER = np.array((1.,.75,0.,.2), dtype=np.float32)
 RED = np.array((1.,0.,0.,.5), dtype=np.float32)
+LIGHT_RED = np.array((1.,0.,0.,.2), dtype=np.float32)
 GREEN = np.array((0.,1.,0.,.5), dtype=np.float32)
+LIGHT_GREEN = np.array((0.,1.,0.,.2), dtype=np.float32)
 BLUE = np.array((0.,0.,1.,.5), dtype=np.float32)
+LIGHT_BLUE = np.array((0.,0.,1.,.2), dtype=np.float32)
 WHITE = np.array((1.,1.,1.,1.), dtype=np.float32)
